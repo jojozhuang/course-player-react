c66f60c6b0f95b17f33429e6d72166a73c9f326c
jojozhuang <jojozhuang@gmail.com>
2021-07-14 20:53:02 -0700

    fix syntax error in webpack.config.prod.js

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -35,21 +35,21 @@ module.exports = {
     rules: [
       {
         test: /\.js?$/,
-        loader: "babel-loader",
+        use: ["babel-loader"],
         exclude: /node_modules/
       },
       {
         test: /\.scss?$/,
-        loader: "style!css!sass",
+        use: ["style!css!sass"],
         include: path.join(__dirname, "src", "styles")
       },
       {
         test: /\.png$/,
-        loader: "file"
+        use: ["file"]
       },
       {
         test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: "file"
+        use: ["file"]
       }
     ]
   }
