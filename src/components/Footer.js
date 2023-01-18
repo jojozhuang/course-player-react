2089ffa62f406fae7fec81da1c36e72c089ac45d
jojozhuang <jojozhuang@gmail.com>
2023-01-17 21:32:10 -0800

    upgrade babel

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
     <div>
       <hr />
       <footer className="container-fluid text-center">
-        <p>&copy; 2021 jojozhuang.github.io, All rights reserved.</p>
+        <p>
+          &copy; {new Date().getFullYear()} jojozhuang.github.io, All rights
+          reserved.
+        </p>
       </footer>
     </div>
   );
